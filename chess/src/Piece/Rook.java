9b5d6049c51afb780530503848a62c61d0bd75b6
thatnastyboi <73314314+thatnastyboi@users.noreply.github.com>
2021-01-11 13:22:08 +0200

    Add files via upload

--- chess/src/Piece/Rook.java
+++ chess/src/Piece/Rook.java
@@ -1,22 +1,23 @@
 package Piece;
 
-public class Rook extends Piece{
+public class Rook extends Piece {
+
     public Rook(boolean iswhite, boolean ispiece) {
         super(iswhite, ispiece);
-        if (iswhite){
+        if (iswhite) {
             setRepresentation("rw ");
-        }
-        else {
+        } else {
             setRepresentation("rb ");
         }
     }
+
     public Point[] moves() {
         Point[] move = new Point[28];
-        for(int i = 1; i<8; i++){
-            move[i-1]=new Point(i,0,false,true,true);
-            move[i+7-1]=new Point(-i,0,false,true,true);
-            move[i+14-1]=new Point(0,i,false,true,true);
-            move[i+21-1]=new Point(0,-i,false,true,true);
+        for (int i = 1; i < 8; i++) {
+            move[i - 1] = new Point(i, 0, true, true, true);
+            move[i + 7 - 1] = new Point(-i, 0, true, true, true);
+            move[i + 14 - 1] = new Point(0, i, true, true, true);
+            move[i + 21 - 1] = new Point(0, -i, true, true, true);
 
         }
         return move;
